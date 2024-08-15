60fe9ef6d076dd1c0427199c71ed2fc7da75bee6
Shashank Yadav <54665960+Skshashank@users.noreply.github.com>
2024-08-15 12:39:32 +0530

    added drawer, tabs and breadcrumbs

---
+++ src/components/Drawer.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import Drawer from '@mui/joy/Drawer';
+import ButtonGroup from '@mui/joy/ButtonGroup';
+import Button from '@mui/joy/Button';
+import List from '@mui/joy/List';
+import Divider from '@mui/joy/Divider';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+
+export default function DrawerCustom() {
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = () => (
+    <Box
+      role="presentation"
+      onClick={toggleDrawer('right', false)}
+      onKeyDown={toggleDrawer('right', false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+          <ListItem key={text}>
+            <ListItemButton>{text}</ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text) => (
+          <ListItem key={text}>
+            <ListItemButton>{text}</ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <React.Fragment>
+      <ButtonGroup variant="outlined">
+        <Button onClick={toggleDrawer('right', true)}>
+          Open Drawer
+        </Button>
+      </ButtonGroup>
+      <Drawer
+        anchor="right"
+        size="lg"
+        open={state['right']}
+        onClose={toggleDrawer('right', false)}
+      >
+        {list()}
+      </Drawer>
+    </React.Fragment>
+  );
+}
